716e4e723e730ab678973c838dc3824037c38fd0
lemonsareprettygood <wizzy.erik@gmail.com>
2023-02-21 15:45:48 +0200

    feat: add sticky footer layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return (
+        <footer className="p-2 bg-indigo-200">Footer</footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return (
+        <header className="p-2 bg-indigo-200">Header</header>
+    );
+}
+
+export default Header;
