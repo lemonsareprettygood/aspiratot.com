e3ca496b73814a4ee94b29ce6c8bff687c8129f0
lemonsareprettygood <wizzy.erik@gmail.com>
2023-02-21 17:26:41 +0200

    fix: correct iframe props

--- src/pages/programare.tsx
+++ src/pages/programare.tsx
@@ -3,7 +3,7 @@ export default function Programare() {
         <div className="bg-blue-300">
             <iframe
                 src="https://docs.google.com/forms/d/e/1FAIpQLSePIeIgkkrIC_2kBy5KIdhjL9dD1989pXKjypLYVHdmLDwgjQ/viewform?embedded=true"
-                width="100%" height="1436" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
+                width="100%" height="1436">Loading…</iframe>
         </div>
     )
 }
