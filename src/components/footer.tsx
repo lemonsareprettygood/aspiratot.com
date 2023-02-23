751fee06e7bb8631ea30ec381964cac0d9dc2eab
lemonsareprettygood <wizzy.erik@gmail.com>
2023-02-23 18:21:36 +0200

    feat: add terms oage

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 const Footer = () => {
     return (
         <footer className="border-t-2 border-t-black p-2 bg-amber-700 text-center text-amber-50 text-lg">
-            Copyright ©️ {new Date().getFullYear()}
+            <div>
+                <Link href="/termeni" className="text-lg mb-2 inline-block">Termeni și Condiții</Link>
+            </div>
+            <p>Copyright ©️ {new Date().getFullYear()}</p>
         </footer>
     );
 }
---
+++ src/pages/termeni.tsx
@@ -0,0 +1,71 @@
+export default function TermeniSiConditii() {
+    return (
+        <div className="prose m-auto py-10">
+            <h1>Termeni și Condiții</h1>
+
+            <h2>Introducere</h2>
+            <p>Această secțiune introduce termenii și condițiile care trebuie acceptate de către client în momentul în
+                care utilizează serviciile de curățenie oferite de firma noastră. Este important ca toți clienții să
+                citească și să înțeleagă acești termeni și condiții înainte de a beneficia de serviciile noastre.</p>
+
+            <h2>Descrierea serviciilor</h2>
+            <p>Această secțiune descrie serviciile de curățenie pe care le oferim. Firma noastră oferă servicii
+                profesionale de curățenie pentru locuințe, birouri și spații comerciale. Ne asigurăm că fiecare serviciu
+                este personalizat în funcție de nevoile specifice ale fiecărui client. Dacă aveți întrebări cu privire
+                la serviciile noastre, suntem mereu disponibili să răspundem la întrebările dumneavoastră.</p>
+
+            <h2>Prețuri și plăți</h2>
+            <p>Această secțiune detaliază prețurile și modalitățile de plată pentru serviciile noastre. Prețurile
+                variază în funcție de tipul de serviciu solicitat și de dimensiunea și complexitatea spațiului. Vom
+                furniza clientului o estimare detaliată a costurilor înainte de începerea lucrărilor. Plata se va
+                efectua în mod obișnuit în avans, prin transfer bancar sau în numerar, în funcție de acordul stabilit
+                între noi și client.</p>
+
+            <h2>Modificări și anulări</h2>
+            <p>Această secțiune explică procedurile pentru modificarea și anularea programărilor pentru serviciile de
+                curățenie. Clientul poate solicita modificări ale programării serviciilor cu cel puțin 24 de ore înainte
+                de data programată. Anularea serviciilor trebuie făcută cu cel puțin 48 de ore înainte de data
+                programată, altfel se va percepe o penalizare.</p>
+
+            <h2>Responsabilități și obligații</h2>
+            <p>Această secțiune stabilește responsabilitățile și obligațiile atât pentru firma noastră, cât și pentru
+                client. Firma noastră este responsabilă pentru serviciile de curățenie oferite, precum și pentru orice
+                daune sau pierderi cauzate de personalul nostru în timpul serviciilor. În cazul în care clientul are
+                obiecții cu privire la calitatea serviciilor oferite, acestea trebuie să fie comunicate în mod imediat
+                pentru a putea fi remediate. Clientul este responsabil pentru asigurarea securității și a siguranței în
+                timpul serviciilor de curățenie, precum și pentru respectarea cerințelor de securitate și sănătate în
+                muncă.</p>
+
+            <h2>Confidențialitatea și protecția datelor</h2>
+            <p>Această secțiune se referăla protecția datelor și confidențialitatea informațiilor personale ale
+                clienților. Firma noastră respectă legislația în vigoare cu privire la protecția datelor personale și se
+                angajează să protejeze confidențialitatea informațiilor personale ale clienților. Orice informație
+                furnizată de către client va fi utilizată numai în scopul prestării serviciilor de curățenie și nu va fi
+                transmisă către terți fără acordul explicit al clientului.</p>
+
+            <h2>Reclamații și litigii</h2>
+            <p>Această secțiune stabilește procedurile de soluționare a reclamațiilor și litigiilor. În cazul în care
+                există nemulțumiri sau obiecții legate de serviciile oferite de firma noastră, clienții sunt încurajați
+                să ne contacteze imediat pentru a putea fi soluționate în cel mai scurt timp posibil. În cazul în care
+                nu se poate ajunge la un acord amiabil, litigiile vor fi soluționate conform legislației în vigoare.</p>
+
+            <h2>Forța majoră</h2>
+            <p>Această secțiune stabilește că fiecare parte este liberă de orice responsabilitate în cazul unor
+                evenimente neprevăzute, cum ar fi cutremure, inundații, războaie, greve etc. Aceste evenimente sunt
+                considerate forță majoră și pot duce la suspendarea temporară a serviciilor fără ca vreo parte să fie
+                obligată la plata unor despăgubiri.</p>
+
+            <h2>Modificări ale termenilor și condițiilor</h2>
+            <p>Această secțiune stabilește că firma noastră își rezervă dreptul de a modifica termenii și condițiile în
+                orice moment. Clientul va fi informat cu privire la orice modificare înainte de a beneficia de
+                serviciile noastre și are dreptul de a refuza serviciile dacă nu este de acord cu noile termeni și
+                condiții.</p>
+
+            <h2>Acordul de acceptare</h2>
+            <p>Această secțiune stabilește că prin utilizarea serviciilor noastre, clientul este considerat a fi de
+                acord cu toți termenii și condițiile menționate mai sus. Prin urmare, este important ca toți clienții să
+                citească și să înțeleagă acești termeni și condiții înainte de a beneficia de serviciile noastre și să
+                semneze un acord de acceptare înainte de începerea lucrărilor.</p>
+        </div>
+    )
+}
