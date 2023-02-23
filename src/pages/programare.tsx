2a55a9ba1927606fab173199f600719b401da74d
lemonsareprettygood <wizzy.erik@gmail.com>
2023-02-23 16:19:43 +0200

    fix: correct google form background

--- src/pages/programare.tsx
+++ src/pages/programare.tsx
@@ -2,7 +2,7 @@ import Footer from "@/components/footer";
 
 export default function Programare() {
     return (
-        <div className="bg-black-300">
+        <div className="bg-blue-400">
             <iframe
                 src="https://docs.google.com/forms/d/e/1FAIpQLSePIeIgkkrIC_2kBy5KIdhjL9dD1989pXKjypLYVHdmLDwgjQ/viewform?embedded=true"
                 width="100%" height="1436">Loading…</iframe>
