48760b3d55d26ef07e01f2594aab49f9540abb91
lemonsareprettygood <wizzy.erik@gmail.com>
2023-02-23 17:35:06 +0200

    fix: make footer text bigger

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="border-t-2 border-t-black p-2 bg-amber-700 text-center text-amber-50">
+        <footer className="border-t-2 border-t-black p-2 bg-amber-700 text-center text-amber-50 text-lg">
             Copyright ©️ {new Date().getFullYear()}
         </footer>
     );
